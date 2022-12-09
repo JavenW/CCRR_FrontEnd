604122c62ff97e9090110e5f87b346fc6eddd780
Yining Chen <yic416@ucsd.edu>
2022-12-09 11:47:35 -0500

    receipe page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,25 +1,46 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function About() {
   return (
     <div className="about">
       <div class="container">
         <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
+            <h1 class="font-weight-light">All receipes</h1>
+            <div class="col-lg-6">
+            <Card style={{ width: '20rem', margin: '0.5rem' }}>
+              <Card.Img variant="top" src="omelette.jpeg" />
+              <Card.Body>
+                <Card.Title>Omelette</Card.Title>
+                <Card.Text>
+                Ingredients: eggs, cheese
+                </Card.Text>
+              </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>...</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Detailed instruction</Card.Link>
+            </Card.Body>
+          </Card>
           </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">About</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
+          <div class="col-lg-6">
+          <Card style={{ width: '20rem', margin: '0.5rem' }}>
+              <Card.Img variant="top" src="omelette.jpeg" />
+              <Card.Body>
+                <Card.Title>Receipe name2</Card.Title>
+                <Card.Text>
+                Ingredients: blabla...
+                </Card.Text>
+              </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>...</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Detailed instruction</Card.Link>
+            </Card.Body>
+          </Card>
           </div>
         </div>
       </div>

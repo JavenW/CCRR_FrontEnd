b6ce476bd144efde1749a0a2108f9785bd53b552
JavenW <513203628@qq.com>
2022-12-08 19:03:53 -0500

    Update Navigation.jsx

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,11 +1,41 @@
-import React from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { LinkContainer } from 'react-router-bootstrap';
 
+import React, {useState} from "react";
+import {useEffect} from "react";
+import jwt_decode from "jwt-decode";
+
 function Navigation(props) {
+  const [user, setUser] = useState({});
+  function handleCallbackResponse(response){
+    console.log("Encoded JWT ID token:" + response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSignOut(event){
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "348914870957-et5471e8skds6e7firt8oglhkhp766ql.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+    google.accounts.id.renderButton(
+        document.getElementById("signInDiv"),
+        {theme: "outline", size:"large"}
+    );
+  },[]);
+
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
@@ -23,7 +53,9 @@ function Navigation(props) {
           </Nav>
           <Nav>
             <NavDropdown title="Account" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="signin">Sign In</NavDropdown.Item>
+              <NavDropdown.Item>
+                <div id = "signInDiv"></div>
+              </NavDropdown.Item>
               <NavDropdown.Item href="signout">
                 Sign On
               </NavDropdown.Item>

a36310553482c4a8eca145f3f12693cdeba698c8
Yining Chen <yic416@ucsd.edu>
2022-12-23 17:25:17 -0500

    add sns button

--- frontend/src/pages/DetailedItem.jsx
+++ frontend/src/pages/DetailedItem.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import { useState, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react';
 import axios from "axios";
 import { useSearchParams } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Overlay from 'react-bootstrap/Overlay';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 function DetailedItem() {
     const [detailedData, setDetailedDate] = useState(null);
     const [queryParameters] = useSearchParams();
+    const [show, setShow] = useState(false);
+    const target = useRef(null);
     let itemname = queryParameters.get('item')
     const parse = require('html-react-parser')
     function getData() {
@@ -40,14 +44,24 @@ function DetailedItem() {
           {/* new line start*/}
             {detailedData && <div>
                 <div className="section"><img src={detailedData.receipe_image} alt="receipeimg" /></div>
-                <hr/>
+                <div className='section'><Button ref={target} onClick={() => setShow(!show)}>
+                Click to send recipe info to your email
+                </Button>
+                <Overlay target={target.current} show={show} placement="right">
+                  {(props) => (
+                    <Tooltip id="overlay-example" {...props}>
+                    Sent!
+                    </Tooltip>
+                  )}
+                </Overlay></div>
                 <div className="section">Receipe name: <b>{detailedData.receipe_name}</b></div>
                 <hr/>
                 <div className="section">Prepare time: {detailedData.receipe_time} min</div>
                 <hr/>
+                <div className="section">Instructions: <br></br>{detailedData.receipe_instruction}</div>
+                <hr/>
                 <div className="section">Summary: <article>{detailedData.receipe_summary}</article></div>
                 <hr/>
-                <div className="section">Instructions: <br></br>{detailedData.receipe_instruction}</div>
             </div>
             }
            {/* end of new line */}
